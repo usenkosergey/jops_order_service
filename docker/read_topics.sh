73abd94688071f7c558b88db919b04032a6a5927
Sergey Usenko <UsenkoS@terralink.ru>
2025-03-16 16:49:42 +0700

    11_kafka_integration_4.patch

--- docker/read_topics.sh
+++ docker/read_topics.sh
@@ -8,3 +8,6 @@ echo -e "\n*** Sampling messages in Kafka topics ***\n"
 
 echo -e "\n-----v1.public.orders_outbox topic-----"
 docker compose exec connect kafka-avro-console-consumer --bootstrap-server $BOOTSTRAP_SERVERS --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --timeout-ms 10000 --max-messages 5 --topic v1.public.orders_outbox
+
+echo -e "\n-----v1.orders_dispatch topic-----"
+docker compose exec connect kafka-avro-console-consumer --bootstrap-server $BOOTSTRAP_SERVERS --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --timeout-ms 10000 --max-messages 5 --topic v1.orders_dispatch
